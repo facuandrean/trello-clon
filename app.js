d708a884d8883448d167b725ae24da1281b47054
facuandrean <facundoandrean22@gmail.com>
2024-03-30 10:54:56 -0300

    Añadiendo primeras estructuras de las distintas vistas

--- app.js
+++ app.js
@@ -2,35 +2,46 @@ import express from "express";
 import path from "path";
 import logger from "morgan";
 import viewController from "./controllers/viewController.js";
+import crypto from 'crypto';
+
 
 const app = express();
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const port = process.env.PORT || 3000;
-const publicPath = path.join(process.cwd(), 'public');
 
 app.use(logger('dev'));
 
-app.set("/views", path.join(process.cwd(), "/views"));
+app.set("/views", path.join(__dirname, "/views"));
 app.set("view engine", "pug");
 
-app.use(express.static(publicPath));
-
-// app.use(express.static("public", { 
-//     setHeaders: (res, path) => {
-//         if (path.endsWith(".css")) {
-//             res.setHeader("Content-Type", "text/css");
-//         }
-//     }
-// }));
+app.use(express.static("public", { 
+    setHeaders: (res, path) => {
+        if (path.endsWith(".css")) {
+            res.setHeader("Content-Type", "text/css");
+        }
+    }
+}));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+
+// Configurar la CSP
+
+// app.use((req, res, next) => {
+//     res.setHeader('Content-Security-Policy', "style-src 'self' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://fonts.googleapis.com 'unsafe-inline'; script-src 'self' https://kit.fontawesome.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net");
+//     next();
+// });
+
 app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "script-src 'self' https://kit.fontawesome.com");
+    res.setHeader('Content-Security-Policy', `style-src 'self' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://fonts.googleapis.com 'unsafe-inline'; script-src 'self' https://kit.fontawesome.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net`);
     next();
 });
 
 app.get("/", viewController.getViewMyBoards);
+app.get("/boards-automatics", viewController.getViewBoardsAutomatics);
+app.get("/calendar", viewController.getViewCalendar);
 
 app.listen(port, () => {
     console.log(`Iniciando app desde http://localhost:${port}`);
--- controllers/viewController.js
+++ controllers/viewController.js
@@ -2,6 +2,16 @@ const getViewMyBoards = (req, res) => {
     res.render("my-boards", { title: "Mi Organizador", titleView: "¡Organiza tus tareas!", descriptionView: "Crea los tableros que necesites para poder organizar, categorizar y lograr realizar todas tus tareas." })
 };
 
+const getViewBoardsAutomatics = (req, res) => {
+    res.render("boards-automatic", { title: "Mi Organizador", titleView: "¡Organiza tus tareas!", descriptionView: "Mantén organizadas tus tareas de manera temporal para que tengas en claro tus objetivos a cumplir." })
+};
+
+const getViewCalendar = (req, res) => {
+    res.render("calendar", { title: "Mi Organizador", titleView: "¡Guarda tus eventos!", descriptionView: "Anota tus próximos eventos para no olvidartelos." })
+};
+
 export default {
-    getViewMyBoards
+    getViewMyBoards,
+    getViewBoardsAutomatics,
+    getViewCalendar
 }
